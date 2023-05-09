1e383ed4a8413d70b5d505201baa9e4d8a717434
ElbertD <diaz.elbert@gmail.com>
2023-05-09 16:03:56 -0400

    variables

---
+++ js_basico/variables.js
@@ -0,0 +1,16 @@
+var nombre = "Elbert";
+/* declarar una variable */
+ var edad;
+ 
+ edad
+ /* inicializar la variable */
+ edad = 30;
+
+ edad
+
+ var elementos = ["computador","Celular","tablet"];
+
+ var persona = {
+    nombre: "Diego",
+    Edad: 30
+ }
