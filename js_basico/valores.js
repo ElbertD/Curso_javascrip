8f86258fdfd9aced4c35be871beff717ea94dbf6
ElbertD <diaz.elbert@gmail.com>
2023-05-09 14:45:25 -0400

    tipos de valores

---
+++ js_basico/valores.js
@@ -0,0 +1,13 @@
+40 /* numeros o numbers */
+"texto" /* texto o string deben ir  con comillas para ser reconocidos */
+true /* valores Boolean */
+false /* valores Boolean */
+
+null /* valores vacios */
+undefined /* valores vacios */
+/* los valores mencionados son los valores primitivos */
+
+/* valores no primitivos o valoes objetos: */
+ [1,2,3]  /* valores array> Cuentan como valores objeto */
+ { nombre: "Elbert" } /* esto seria un objeto */
+ 
